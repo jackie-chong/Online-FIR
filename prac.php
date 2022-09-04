a2bc858c6965db02345cfc6e3a035f1c2faf8659
rupali126 <112821648+rupali126@users.noreply.github.com>
2022-09-04 23:31:19 +0530

    added database html files

---
+++ prac.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html>
+     <head> 
+    <title> police complain </title>
+    </head>
+
+  <body>
+    <h1> First heading </h1>
+    <h2> second heading </h2>
+    <h3> third heading </h3>
+    <h4> Four heading </h4>
+    <h5> Five heading </h5>
+    <h6> six  heading </h6>
+    <p> government of police hasdhaukdjakjbndcb </p>
+
+    
+    <h1> marks sheet  </h1>
+    <table  border =" 1 " >
+      <tr>
+        <th>  English </th>
+        <th>  hindi </th>
+        <th>  math </th>
+        <th>  physics </th>
+        <th>  chemistry </th>
+
+      </tr>
+      <tr>
+        <td> 60 </td>
+        <td> 70 </td>
+        <td> 80 </td>
+        <td> 79 </td>
+        <td> 80 </td>
+      </tr>
+      <tr>
+        <td> 60 </td>
+        <td> 70 </td>
+        <td> 80 </td>
+        <td> 79 </td>
+        <td> 80 </td>
+      </tr>
+      <tr>
+        <td> 60 </td>
+        <td> 70 </td>
+        <td> 80 </td>
+        <td> 79 </td>
+        <td> 80 </td>
+      </tr>
+      <tr>
+        <td> 60 </td>
+        <td> 70 </td>
+        <td> 80 </td>
+        <td> 79 </td>
+        <td> 80 </td>
+      </tr>
+      <tr>
+        <td> 60 </td>
+        <td> 70 </td>
+        <td> 80 </td>
+        <td> 79 </td>
+        <td> 80 </td>
+      </tr>
+
+
+        
+    
+    </table>
+    <h3> Registration form </h3>
+
+    <form>
+      First name:<input type ="text "  placeholder=" enter your name" > <br>
+      Last name:<input type ="text " placeholder=" enter your name"> <br>
+      password: <input type = "password" placeholder=" enter your password"> <br>
+      phone number: <input type"text" placeholder="enter your number"> <br>
+
+     select gender <input type="radio" name="gender">male
+      <input type="radio" name="gender">female <br>
+
+    <button type="submit" value="submit"> submit  form </button>
+
+
+
+    </form>
+    
+  </body>
+
+ </html>
+
+
+
